dd01155dd069e11542b52bdc2893a098e814449c
allerria <wtf496@gmail.com>
2019-05-20 17:04:41 +0900

    r2, quantile_loss metrics for regression.

--- metrics/__init__.py
+++ metrics/__init__.py
@@ -1,5 +1,4 @@
 from .classification import accuracy
-from .regression import mean_squared_error, mean_absolute_error, root_mean_squared_error
+from .regression import mean_squared_error, mean_absolute_error, root_mean_squared_error, r2, quantile_loss
 
-__all__ = ['mean_squared_error', 'mean_absolute_error',
-           'root_mean_squared_error', 'accuracy']
+__all__ = ['mean_squared_error', 'mean_absolute_error', 'root_mean_squared_error', 'accuracy', 'r2', 'quantile_loss']
--- metrics/regression.py
+++ metrics/regression.py
@@ -16,3 +16,15 @@ def mean_absolute_error(y: np.array, y_pred: np.array) -> np.float:
 def root_mean_squared_error(y: np.array, y_pred: np.array) -> np.float:
     y, y_pred = check_dims(y, y_pred)
     return np.sqrt(np.mean(np.square((y_pred - y))))
+
+
+def r2(y: np.array, y_pred: np.array) -> np.float:
+    y, y_pred = check_dims(y, y_pred)
+    return 1 - mean_squared_error(y, y_pred) / y.std()
+
+
+def quantile_loss(y: np.array, y_pred: np.array, t: np.float = 0.5) -> np.float:
+    y, y_pred = check_dims(y, y_pred)
+    n = y.shape[0]
+    loss = np.sum(list(map(lambda y, y_pred: ((t - 1) * y < y_pred + t * y >= y_pred) * (y - y_pred), y, y_pred))) / n
+    return loss
