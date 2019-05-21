997c542a96d35bdde8bf2dcf7344adf4fb8a0458
allerria <wtf496@gmail.com>
2019-05-21 14:52:06 +0900

    precision, recall, f1, fbeta.

--- metrics/__init__.py
+++ metrics/__init__.py
@@ -1,4 +1,5 @@
-from .classification import accuracy
+from .classification import accuracy, precision, recall, f1, fbeta
 from .regression import mean_squared_error, mean_absolute_error, root_mean_squared_error, r2, quantile_loss
 
-__all__ = ['mean_squared_error', 'mean_absolute_error', 'root_mean_squared_error', 'accuracy', 'r2', 'quantile_loss']
+__all__ = ['mean_squared_error', 'mean_absolute_error', 'root_mean_squared_error', 'accuracy', 'r2', 'quantile_loss',
+           'precision', 'recall', 'f1', 'fbeta']
--- metrics/classification.py
+++ metrics/classification.py
@@ -1,7 +1,33 @@
 import numpy as np
+
 from utils import check_dims
 
 
 def accuracy(y: np.array, y_pred: np.array) -> np.float:
     y, y_pred = check_dims(y, y_pred)
     return np.sum(y == y_pred) / y.shape[0]
+
+
+def precision(y: np.array, y_pred: np.array) -> np.float:
+    y, y_pred = check_dims(y, y_pred)
+    tp = np.sum(y == 1 & y_pred == 1)
+    fp = np.sum(y != 1 & y_pred == 1)
+    return tp / (tp + fp)
+
+
+def recall(y: np.array, y_pred: np.array) -> np.float:
+    y, y_pred = check_dims(y, y_pred)
+    tp = np.sum(y == 1 & y_pred == 1)
+    fn = np.sum(y == 1 & y_pred != 1)
+    return tp / (tp + fn)
+
+
+def fbeta(y: np.array, y_pred: np.array, beta: np.float) -> np.float:
+    y, y_pred = check_dims(y, y_pred)
+    p = precision(y, y_pred)
+    r = recall(y, y_pred)
+    return (1 + beta ** 2) * p * r / (beta ** 2 * p + r)
+
+
+def f1(y: np.array, y_pred: np.array) -> np.float:
+    return fbeta(y, y_pred, 1)
